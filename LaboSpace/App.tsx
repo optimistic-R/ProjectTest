dccdd34c598f3a2d6342044ca3a449567020c338
R'sWorkStation <148614795+ optimistic-R@users.noreply.github.com>
2026-03-15 02:50:55 +0900

    SetUp Eslint

--- LaboSpace/App.tsx
+++ LaboSpace/App.tsx
@@ -8,7 +8,7 @@ export default function App() {
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
@@ -18,4 +18,4 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+})
