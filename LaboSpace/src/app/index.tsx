9d486ebe25bd647a7cfe6f7c83ea296b2b3a87ed
R'sWorkStation <148614795+ optimistic-R@users.noreply.github.com>
2026-03-22 11:15:49 +0900

    Markup memo list screen

--- LaboSpace/src/app/index.tsx
+++ LaboSpace/src/app/index.tsx
@@ -1,11 +1,62 @@
-import { View,Text } from "react-native";
+import { View,Text,StyleSheet } from "react-native";
 
 const Index = () => {
     return (
-        <View>
-            <Text>Hello World</Text>
+        <View style={styles.container}>{/*//全体のフレーム*/}
+
+            <View>{/*//headerのフレーム*/}
+                <View>{/*//headerの構造化*/}
+                    <Text>LaboSpace</Text>
+                    <Text>ログアウト</Text>
+                </View>
+            </View>
+
+            <View>{/*//memoListの大枠*/}
+
+                <View>{/*//memoItemの構造化*/}
+                    <View>
+                        <Text>買い物リスト</Text>
+                        <Text>2026/03/22/11:00</Text>
+                    </View>
+                    <View>
+                        <Text>X</Text>
+                    </View>
+                </View>
+
+                <View>{/*//memoItemの構造化*/}
+                    <View>
+                        <Text>買い物リスト</Text>
+                        <Text>2026/03/22/11:00</Text>
+                    </View>
+                    <View>
+                        <Text>X</Text>
+                    </View>
+                </View>
+
+                <View>{/*//memoItemの構造化*/}
+                    <View>
+                        <Text>買い物リスト</Text>
+                        <Text>2026/03/22/11:00</Text>
+                    </View>
+                    <View>
+                        <Text>X</Text>
+                    </View>
+                </View>
+                
+            </View>
+
+            <View>{/*memoListの追加ボタン*/}
+                <Text>＋</Text>
+            </View>
         </View>
     )
 }
 
+const styles = StyleSheet.create({
+    container:{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    }
+})
 export default Index
