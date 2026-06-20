b62efc8c1f133abf81238f334712099d7eae6968
optimistic-R <148614795+optimistic-R@users.noreply.github.com>
2026-06-20 16:26:42 +0900

    Add a feature to save user notes to the database

--- LaboSpace/src/app/memo/create.tsx
+++ LaboSpace/src/app/memo/create.tsx
@@ -1,24 +1,59 @@
 import { 
-    View, TextInput, StyleSheet, KeyboardAvoidingView
- } from "react-native";
-import { router } from "expo-router";
+    View, TextInput, StyleSheet
+ } from "react-native"
+import { router } from "expo-router"
+import { collection, addDoc, Timestamp } from "firebase/firestore"
+import { useState } from "react"
 
-import CircleButton from "../../components/CircleBtton";
-import Icon from "../../components/Icon";
+import KeyboardAvoidingView from "../../components/KeyboardAvoidingView"
+import CircleButton from "../../components/CircleBtton"
+import Icon from "../../components/Icon"
+import { db, auth } from "../../config"
 
-const handlePress = (): void => {
-    router.back();
+const handlePress = /*async*/ (bodyText: string): void => {
+    if(auth.currentUser === null) return
+    const ref = collection(db, `users/${auth.currentUser?.uid}/memos`)
+
+    //データの保存
+    addDoc(ref,{
+        bodyText,
+        updatedAt: Timestamp.fromDate(new Date())
+    })
+    .then((docRef) => {
+        console.log("データの保存に成功しました", docRef.id)
+        router.back()
+    })
+    .catch(() => {
+        console.log("データの保存に失敗しました")
+    })
+
+/*
+    await addDoc(collection(db, "memos"),{
+        bodyText: "test2"
+    })
+    .catch(() =>{
+        console.log("データの保存に失敗しました")
+    })
+    router.back()
+*/
 }
 
 const Create = ()=> {
+    const [bodyText, setBodyText] = useState("")
     return(
-        <KeyboardAvoidingView behavior="height" style={styles.container}>
+        <KeyboardAvoidingView  style={styles.container}>
 
             <View style={styles.InputContainer}>
-                <TextInput multiline style={styles.Input} value="" /> {/* multilineは複数行入力を可能にするプロパティ */}
+                <TextInput 
+                multiline
+                style={styles.Input}
+                value={ bodyText} 
+                onChangeText={(text) => { setBodyText(text) }}
+                autoFocus //画面が表示されたときに自動でフォーカスするプロパティ
+                /> {/* multilineは複数行入力を可能にするプロパティ */}
             </View>
 
-            <CircleButton onPress={handlePress}>
+            <CircleButton onPress={ () => handlePress(bodyText)}>
                 <Icon name="check" size={40} color="#ffff" />
             </CircleButton>
         </KeyboardAvoidingView>
